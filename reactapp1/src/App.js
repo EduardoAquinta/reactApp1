9e4567a20e019389598cfd9e1e18528ed41c6766
EduardoAquinta <edwoodashton@yahoo.co.uk>
2022-05-31 11:18:59 +0100

    everything set up and startred the Container image

---
+++ reactapp1/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+
+
+function Container() {
+
+}
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header/>
+    </div>
+  );
+}
+
+export default App;   
